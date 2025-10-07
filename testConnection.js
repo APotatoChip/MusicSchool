7307dc90b3efd3bbe2c7a19975df884b3a3fb7bd
APotatoChip <cvetina.a.m@gmail.com>
2025-10-07 21:57:34 +0300

    initial setup

---
+++ testConnection.js
@@ -0,0 +1,20 @@
+require("dotenv").config(); // Load .env variables
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting:", err.message);
+    return;
+  }
+  console.log("Connected to MySQL successfully!");
+});
+
+connection.end();
